3e97ef2306a85e73a768b2e04e28e6ce3dd6e8d3
nsoksh <nsoksh982@gmail.com>
2024-12-14 16:38:04 +0900

    initial commit

---
+++ paiza.py
@@ -0,0 +1,24 @@
+##Python3入門編
+
+#標準出力
+print ("hello world")
+
+#変数を使う
+player = "勇者"
+
+#変数の出力
+print(player + "は、荒野を歩いていた")
+
+#型変換
+str(100)
+
+#ランダム関数
+#0から1までのランダムな数値を出力する
+random.random()
+# aからbまでの間のランダムな整数を出力する
+random.randint(a, b)
+
+#演習課題「1から6のサイコロを作る」
+import random
+num = random.randint(1,6)
+print("サイコロの目は" + str(num) + "です。")
